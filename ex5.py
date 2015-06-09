a7f90224dbcdd51ff87675120c05b98e5101a873
Joe Siami <siamijw@gmail.com>
2015-06-08 20:26:24 -0700

    initial project version

---
+++ ex5.py
@@ -0,0 +1,19 @@
+
+name = 'Joe Siami'
+age = 21
+height = 68 #inches
+weight = 175 #lbs
+eyes = 'brown'
+teeth = 'white'
+hair = 'brown'
+
+print "let's talk about %s." % name
+print "He's %f feet tall." % height/12
+print "He's %f kilograms heavy." % weight* .453
+print "Actually that's not too heavy"
+print "he's got %s eyes and %s hair" % (eyes, hair)
+print 'his teeth are usually %s depending on the coffee.' % teeth
+
+print "If I add %d, %d, and %d I get %d." % (
+	age, height, weight,  age + height + weight)
+
